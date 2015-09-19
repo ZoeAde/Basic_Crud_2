dd3afac909e2a47b810462d7abd14f8f9bbeac55
ZoeAde <zoe.a.adelman@gmail.com>
2015-09-19 14:17:07 -0600

    adding routes

--- server/app.js
+++ server/app.js
@@ -10,7 +10,7 @@ var swig = require('swig');
 
 // *** routes *** //
 var routes = require('./routes/index.js');
-
+var contacts = require('./routes/api.js');
 
 // *** express instance *** //
 var app = express();
@@ -36,7 +36,7 @@ app.use(express.static(path.join(__dirname, '../client/public')));
 
 // *** main routes *** //
 app.use('/', routes);
-
+app.use('/api/', contacts);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- server/routes/api.js
+++ server/routes/api.js
@@ -3,18 +3,63 @@ var router = express.Router();
 var Contact = require('../models/contacts.js');
 
 //get all contacts
-
+router.get('/contacts', function(req, res, next) {
+  Contact.find(function(err, contacts) {
+    if (err) {
+      res.json({'message': err});
+    } else {
+      res.json(contacts);
+    }
+  });
+});
 
 //get single contact
-
+router.get('/contact/:id', function(req, res, next) {
+  Contact.findById(req.params.id, function(err, contact) {
+    if (err) {
+      res.json({'message': err});
+    } else {
+      res.json(contact);
+    }
+  });
+});
 
 //post all contacts
-
+router.post('/contacts', function(req, res, next){
+  var newContact = new Contact({
+    name: req.body.name,
+    address: req.body.address,
+    profession: req.body.profession
+  });
+  newContact.save(function(err, contacts){
+    if (err) {
+      res.json({'message': err});
+    } else {
+      res.json(contacts);
+    }
+  });
+});
 
 //put one contact
-
+router.put('/contact/:id', function(req, res, next) {
+  Contact.findByIdAndUpdate(function(err, contact) {
+    if (err) {
+      res.json({'message': err});
+    } else {
+      res.json(contact);
+    }
+  });
+});
 
 //delete one contact
-
+router.delete('/contact/:id', function(req, res, next) {
+  Contact.findByIdAndRemove(function(err, contacts) {
+    if (err) {
+      res.json({'message': err});
+    } else {
+      res.json(contacts);
+    }
+  });
+});
 
 module.exports = router;
